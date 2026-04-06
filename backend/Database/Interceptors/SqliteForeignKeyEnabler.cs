f9b6a52c84439ebf5b5750c69558ae8b89a120ff
arubi9 <arubirubi9@gmail.com>
2026-04-06 15:04:39 -0400

    Guard SQLite-specific PRAGMAs from running on PostgreSQL

--- backend/Database/Interceptors/SqliteForeignKeyEnabler.cs
+++ backend/Database/Interceptors/SqliteForeignKeyEnabler.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Common;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace NzbWebDAV.Database.Interceptors;
@@ -7,6 +8,8 @@ public class SqliteForeignKeyEnabler : DbConnectionInterceptor
 {
     public override void ConnectionOpened(DbConnection connection, ConnectionEndEventData eventData)
     {
+        if (connection is not SqliteConnection) return;
+
         using var command = connection.CreateCommand();
         command.CommandText = "PRAGMA foreign_keys = ON;";
         command.ExecuteNonQuery();
