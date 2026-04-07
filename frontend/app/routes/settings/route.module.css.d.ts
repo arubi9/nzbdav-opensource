047441e746c843ed0c1d6e06d10045b43fb0c58c
arubi9 <arubirubi9@gmail.com>
2026-04-07 10:42:03 -0400

    feat: surface encryption status in settings

--- frontend/app/routes/settings/route.module.css.d.ts
+++ frontend/app/routes/settings/route.module.css.d.ts
@@ -1,7 +1,11 @@
 declare const styles: {
+  readonly "banner": string;
+  readonly "bannerButton": string;
+  readonly "bannerInfo": string;
+  readonly "bannerTitle": string;
+  readonly "bannerWarning": string;
   readonly "container": string;
   readonly "tabs": string;
   readonly "button": string;
 };
 export = styles;
-
